c14acef6dee36badea04c1ac261ee45e8b746696
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-02-06 13:21:23 +0530

    added main page

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello, JavaScript!");
+console.log("This is a simple JavaScript file.");
+console.log("It demonstrates basic logging to the console.");
+console.log("You can run this file using Node.js or in a web browser console.");
+console.log("Feel free to modify and expand upon this code!");
+console.log("Happy coding!");   
